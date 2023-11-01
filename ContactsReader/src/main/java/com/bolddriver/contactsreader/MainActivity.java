bddb05798e3d98dcd82c1cbef601e6cec12c06a8
Bolddriver <86687241+Bolddriver@users.noreply.github.com>
2023-11-01 09:53:56 +0800

    联系人读取器

--- ContactsReader/src/main/java/com/bolddriver/contactsreader/MainActivity.java
+++ ContactsReader/src/main/java/com/bolddriver/contactsreader/MainActivity.java
@@ -1,14 +1,72 @@
 package com.bolddriver.contactsreader;
 
+import android.Manifest;
+import android.content.ContentResolver;
+import android.content.pm.PackageManager;
+import android.database.Cursor;
+import android.os.Bundle;
+import android.provider.ContactsContract;
+import android.util.Log;
+import android.widget.ListView;
+import android.widget.SimpleCursorAdapter;
+
 import androidx.appcompat.app.AppCompatActivity;
 
-import android.os.Bundle;
-
 public class MainActivity extends AppCompatActivity {
+
+    private static final String TAG = "GetContacts";
+
+    private ListView mListView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        mListView = findViewById(R.id.list_view);
+
+        // 检查应用是否具有读取联系人的权限
+        if (checkSelfPermission(Manifest.permission.READ_CONTACTS) != PackageManager.PERMISSION_GRANTED) {
+            requestPermissions(new String[]{Manifest.permission.READ_CONTACTS}, 1);
+            return;
+        }
+
+        // 获取联系人信息
+        Log.d(TAG, "onCreate: 获取联系人信息");
+
+        ContentResolver resolver = getContentResolver();
+        //联系人姓名 联系人号码 按升序排列
+        Cursor cursor = resolver.query(ContactsContract.CommonDataKinds.Phone.CONTENT_URI, null, null, null, ContactsContract.CommonDataKinds.Phone.DISPLAY_NAME + " ASC");
+
+        // 创建简单适配器，将联系人信息显示在列表视图中
+        SimpleCursorAdapter adapter = new SimpleCursorAdapter(
+                this,
+                R.layout.list_item,
+                cursor,
+                new String[]{ContactsContract.CommonDataKinds.Phone.DISPLAY_NAME, ContactsContract.CommonDataKinds.Phone.NUMBER},
+                new int[]{R.id.name, R.id.number}
+        );
+        mListView.setAdapter(adapter);
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, String[] permissions, int[] grantResults) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+
+        if (requestCode == 1 && grantResults.length > 0 && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+            // 获取联系人信息
+            ContentResolver resolver = getContentResolver();
+            Cursor cursor = resolver.query(ContactsContract.CommonDataKinds.Phone.CONTENT_URI, null, null, null, ContactsContract.CommonDataKinds.Phone.DISPLAY_NAME + " ASC");
+
+            // 创建简单适配器，将联系人信息显示在列表视图中
+            SimpleCursorAdapter adapter = new SimpleCursorAdapter(
+                    this,
+                    R.layout.list_item,
+                    cursor,
+                    new String[]{ContactsContract.CommonDataKinds.Phone.DISPLAY_NAME, ContactsContract.CommonDataKinds.Phone.NUMBER},
+                    new int[]{R.id.name, R.id.number}
+            );
+            mListView.setAdapter(adapter);
+        }
     }
 }
