404e537843b1f8981d2e538c3fc835823ee1d395
Bolddriver <86687241+Bolddriver@users.noreply.github.com>
2023-11-01 20:14:10 +0800

    联系人拦截器：listview，自定义listview adapter，将选中的联系人插入ContactsHooker的ContentProvider

--- ContactsHooker/src/main/java/com/bolddriver/contactshooker/MainActivity.java
+++ ContactsHooker/src/main/java/com/bolddriver/contactshooker/MainActivity.java
@@ -1,40 +1,104 @@
 package com.bolddriver.contactshooker;
 
+import android.Manifest;
 import android.annotation.SuppressLint;
+import android.content.ContentResolver;
 import android.content.ContentValues;
+import android.content.pm.PackageManager;
 import android.database.Cursor;
 import android.os.Bundle;
+import android.provider.ContactsContract;
 import android.util.Log;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.CheckBox;
+import android.widget.ListAdapter;
+import android.widget.ListView;
+import android.widget.SimpleCursorAdapter;
+import android.widget.TextView;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.RecyclerView;
 
 import com.bolddriver.contactshooker.provider.ContactsInfoContent;
 
 public class MainActivity extends AppCompatActivity {
     private static String TAG = "contactsBold";
+    private ListView mListView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        mListView = findViewById(R.id.list_view);
 
-        try {
-            ContentValues values = new ContentValues();
-            values.put(ContactsInfoContent.C_NAME, "hello");
-            values.put(ContactsInfoContent.C_NUMBER, "123123123123");
-            getContentResolver().insert(ContactsInfoContent.CONTENT_URI, values);
-            Log.d(TAG, "insert成功");
+        // 检查应用是否具有读取联系人的权限
+        if (checkSelfPermission(android.Manifest.permission.READ_CONTACTS) != PackageManager.PERMISSION_GRANTED) {
+            requestPermissions(new String[]{Manifest.permission.READ_CONTACTS}, 1);
+            return;
+        }
 
-            Cursor cursor = getContentResolver().query(ContactsInfoContent.CONTENT_URI, null, null, null, null);
-            if (cursor != null) {
-                while (cursor.moveToNext()) {
-                    @SuppressLint("Range") String name = cursor.getString(cursor.getColumnIndex(ContactsInfoContent.C_NAME));
-                    @SuppressLint("Range") String number = cursor.getString(cursor.getColumnIndex(ContactsInfoContent.C_NUMBER));
-                    Log.d(TAG, "name:" + name + " numebr:" + number);
+        //查询联系人信息
+        ContentResolver resolver = getContentResolver();
+        Cursor cursor = resolver.query(ContactsContract.CommonDataKinds.Phone.CONTENT_URI, null, null, null, ContactsContract.CommonDataKinds.Phone.DISPLAY_NAME + " ASC");
+
+        // 创建适配器，将联系人信息显示在列表视图中
+        MyAdapter adapter = new MyAdapter(cursor, this);
+        mListView.setAdapter(adapter);
+
+        //保存按钮的点击事件
+        Button btn_save = findViewById(R.id.btn_save);
+        btn_save.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                int count = mListView.getCount();
+                for (int i = 0; i < count; i++) {
+                    View view = mListView.getChildAt(i);
+                    CheckBox checkBox = view.findViewById(R.id.cb_itemChecked);
+                    if (checkBox.isChecked()) {
+                        // 获取选中项的联系人信息
+                        cursor.moveToPosition(i);
+                        @SuppressLint("Range") String name = cursor.getString(cursor.getColumnIndex(ContactsContract.CommonDataKinds.Phone.DISPLAY_NAME));
+                        @SuppressLint("Range") String number = cursor.getString(cursor.getColumnIndex(ContactsContract.CommonDataKinds.Phone.NUMBER));
+                        // 打印联系人信息
+                        Log.d(TAG, "Name: " + name + ", Number: " + number);
+                        //将选中的联系人插入ContentProvider
+                        ContentValues values = new ContentValues();
+                        values.put(ContactsInfoContent.C_NAME, name);
+                        values.put(ContactsInfoContent.C_NUMBER, number);
+                        getContentResolver().insert(ContactsInfoContent.CONTENT_URI, values);
+                        Log.d(TAG, "insert成功");
+                    }
+                }
+                //logd输出ContentProvider中的联系人
+                Log.d(TAG, "本地的联系人");
+                Cursor mCursor = getContentResolver().query(ContactsInfoContent.CONTENT_URI, null, null, null, null);
+                if (mCursor != null) {
+                    while (mCursor.moveToNext()) {
+                        @SuppressLint("Range") String name = mCursor.getString(mCursor.getColumnIndex(ContactsInfoContent.C_NAME));
+                        @SuppressLint("Range") String number = mCursor.getString(mCursor.getColumnIndex(ContactsInfoContent.C_NUMBER));
+                        Log.d(TAG, "name:" + name + " numebr:" + number);
+                    }
                 }
             }
-        } catch (Exception e) {
-            Log.d(TAG, "exception:" + e);
+        });
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, String[] permissions, int[] grantResults) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+
+        if (requestCode == 1 && grantResults.length > 0 && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+            //查询联系人信息
+            ContentResolver resolver = getContentResolver();
+            Cursor cursor = resolver.query(ContactsContract.CommonDataKinds.Phone.CONTENT_URI, null, null, null, ContactsContract.CommonDataKinds.Phone.DISPLAY_NAME + " ASC");
+
+            // 创建适配器，将联系人信息显示在列表视图中
+            MyAdapter adapter = new MyAdapter(cursor, this);
+            mListView.setAdapter(adapter);
         }
     }
+
 }
---
+++ ContactsHooker/src/main/java/com/bolddriver/contactshooker/MyAdapter.java
@@ -0,0 +1,93 @@
+package com.bolddriver.contactshooker;
+
+import android.annotation.SuppressLint;
+import android.content.Context;
+import android.database.Cursor;
+import android.provider.ContactsContract;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.BaseAdapter;
+import android.widget.CheckBox;
+import android.widget.TextView;
+
+import com.bolddriver.contactshooker.provider.ContactsInfoContent;
+
+import org.w3c.dom.Text;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+public class MyAdapter extends BaseAdapter {
+    private List<String> name;
+    private List<String> number;
+    private Context context;
+    private Map<Integer,Boolean> map=new HashMap<>();
+    @SuppressLint("Range")
+    public MyAdapter(Cursor cursor, Context context){
+        this.name = new ArrayList<String>();
+        this.number = new ArrayList<String>();
+        this.context=context;
+        if(cursor!=null){
+            int pos=0;
+            while(cursor.moveToNext()){
+                String iname = cursor.getString(cursor.getColumnIndex(ContactsContract.CommonDataKinds.Phone.DISPLAY_NAME));
+                String inumber = cursor.getString(cursor.getColumnIndex(ContactsContract.CommonDataKinds.Phone.NUMBER));
+                this.name.add(iname);
+                this.number.add(inumber);
+            }
+        }
+    }
+    @Override
+    public int getCount() {
+        //return返回的是int类型，也就是页面要显示的数量。
+        return name.size();
+    }
+
+    @Override
+    public Object getItem(int position) {
+        return null;
+    }
+
+    @Override
+    public long getItemId(int position) {
+        return 0;
+    }
+
+    @Override
+    public View getView(final int position, View convertView, ViewGroup parent) {
+        View view;
+        if (convertView==null){
+            //通过一个打气筒 inflate 可以把一个布局转换成一个view对象
+            view=View.inflate(context,R.layout.list_item,null);
+        }else {
+            view=convertView;//复用历史缓存对象
+        }
+        //单选按钮的文字
+        TextView radioText=(TextView)view.findViewById(R.id.name);
+        radioText.setText(name.get(position));
+        TextView radioNumber = (TextView)view.findViewById(R.id.number);
+        radioNumber.setText(number.get(position));
+        //单选按钮
+        final CheckBox checkBox=(CheckBox)view.findViewById(R.id.cb_itemChecked);
+        checkBox.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if (checkBox.isChecked()){
+                    map.put(position,true);
+
+                }else {
+                    map.remove(position);
+
+                }
+            }
+        });
+        if(map!=null&&map.containsKey(position)){
+            checkBox.setChecked(true);
+        }else {
+            checkBox.setChecked(false);
+        }
+        return view;
+    }
+}
